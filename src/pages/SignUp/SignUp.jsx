a78e2e53dbb36e836ae6b1459a10336fe3dae2a8
NahidNM <mdnahidhasan171@gmail.com>
2023-06-07 23:55:54 +0600

    signUp

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -6,6 +6,46 @@ const SignUp = () => {
   const { createUser, updateUserProfile } = useAuth()
   const navigate = useNavigate();
   
+  const onSubmit = data => {
+
+    createUser(data.email, data.password)
+        .then(result => {
+
+            const loggedUser = result.user;
+            console.log(loggedUser);
+
+            updateUserProfile(data.name, data.photoURL)
+                .then(() => {
+                    const saveUser = { name: data.name, email: data.email }
+                    fetch('https://bistro-boss-server-fawn.vercel.app/users', {
+                        method: 'POST',
+                        headers: {
+                            'content-type': 'application/json'
+                        },
+                        body: JSON.stringify(saveUser)
+                    })
+                        .then(res => res.json())
+                        .then(data => {
+                            if (data.insertedId) {
+                                reset();
+                                Swal.fire({
+                                    position: 'top-end',
+                                    icon: 'success',
+                                    title: 'User created successfully.',
+                                    showConfirmButton: false,
+                                    timer: 1500
+                                });
+                                navigate('/');
+                            }
+                        })
+
+
+
+                })
+                .catch(error => console.log(error))
+        })
+};
+  
     return (
         <div>
         <div className="min-h-screen hero bg-base-200">
